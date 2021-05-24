1669510e608f5f9ea92905292e1b80db113ecc1b
Thom711 <tvdveldt@hotmail.com>
2021-05-24 20:18:42 +0200

    test feedback

--- test_main.py
+++ test_main.py
@@ -11,15 +11,18 @@ def client():
 def test_redirect(client):
     response = client.get('/home')
     assert response.status_code == 302
+    print('test_redirect')
 
 
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'<h2>Welcome to the final assignment of Winc Academy!</h2>' in response.data
+    print('test_index')
 
 
 def test_about(client):
     response = client.get('/about')
     assert response.status_code == 200
     assert b'<h2>About me:</h2>' in response.data
+    print('test_about')
